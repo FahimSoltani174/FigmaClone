da4d381b7118965acd86d7066e9257b1171947a5
ahmad <fahim174soltani@gmail.com>
2024-05-20 11:13:08 +0430

    I have successfully add the live chate optoin to the main application

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,38 +1,73 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import LiveCursors from "./cursor/LiveCursors";
 import { useMyPresence, useOthers } from "@/liveblocks.config";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
 
 const Live = () => {
     const others = useOthers();
-    const [{ cursor } , updateMyPresence] = useMyPresence() as any;
-    const handlePointerMove = useCallback((event:React.PointerEvent)=>{
+    const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+    const [cursorState, setCursorState] = useState({
+        mode: CursorMode.Hidden
+    })
+
+    const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
         const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-        
+
         event.preventDefault();
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-        updateMyPresence({cursor: {x , y}})
+        updateMyPresence({ cursor: { x, y } })
 
-    } , []);
+    }, []);
 
-    const handlePointerLeave = useCallback((event:React.PointerEvent)=>{
-        event.preventDefault();
+    const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+        setCursorState({ mode: CursorMode.Hidden })
 
+        updateMyPresence({ cursor: null, message: null })
 
-        updateMyPresence({cursor: null, message: null})
+    }, []);
 
-    } , []);
-
-    const handlePointerDown = useCallback((event:React.PointerEvent)=>{
+    const handlePointerDown = useCallback((event: React.PointerEvent) => {
         const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-        
+
         event.preventDefault();
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-        updateMyPresence({cursor: {x , y}})
+        updateMyPresence({ cursor: { x, y } })
 
-    } , []);
+    }, []);
+
+    useEffect(() => {
+        const onKeyUp = (e: KeyboardEvent) => {
+            if (e.key === "/") {
+                setCursorState({
+                    mode: CursorMode.Chat,
+                    previousMessage: null,
+                    message: ''
+                })
+            } else if (e.key === "Escape") {
+                updateMyPresence({ message: '' })
+                setCursorState({ mode: CursorMode.Hidden })
+            }
+        }
+        const onKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "/") {
+                e.preventDefault();
+            }
+        }
+
+        window.addEventListener('keyup', onKeyUp);
+        window.addEventListener('keydown', onKeyDown);
+
+        return () => {
+            window.removeEventListener('keyup', onKeyUp);
+            window.removeEventListener('keydown', onKeyDown);
+        }
+
+    }, [updateMyPresence]);
     return (
         <div
             onPointerMove={handlePointerMove}
@@ -41,6 +76,15 @@ const Live = () => {
             className="border-2 border-green-500 h-[100vh] w-full flex justify-center items-center text-center"
         >
             <h1 className="tet-2xl text-white">Live Blocks Figma Clone</h1>
+
+            {cursor && (
+                <CursorChat
+                    cursor={cursor}
+                    cursorState={cursorState}
+                    setCursorState={setCursorState}
+                    updateMyPresence={updateMyPresence}
+                />
+            )}
             <LiveCursors others={others} />
         </div>
     )
--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -11,7 +11,11 @@ const Cursor = ({color, x, y, message}: Props) => {
     return (
         <div className="pointer-events-nonea absolute top-0 left-0" style ={{transform: `translateX(${x}px) translateY(${y}px)`}}>
             <CursorSVG color = {color}/>
-            {/* Message */}
+            {message &&(
+                <div className="absolute left-2 top-5 rounded-xl px-4 py-2" style={{backgroundColor: color}}>
+                    <p className="text-white whitespace-nowrap text-sm leading-relaxed">{message}</p>
+                </div>
+            )}
         </div>
     )
 }
